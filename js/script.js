c86a234cadb521363ec2b8ee40ec8a1ef531b256
Pukhovskiy Denis <d.pukhovskiy@office.local>
2018-02-06 15:03:50 +0300

    club page added

--- js/script.js
+++ js/script.js
@@ -17,16 +17,6 @@ if ($('#home-v1-testimonial-slider').length) {
 $(document).ready(function(){
 	
 	
-	$("#trainers-gallery-slider .owl-carousel").owlCarousel({
-		singleItem: true,
-        stopOnHover: true,
-        slideSpeed: 400,
-        pagination: true,
-        mouseDrag: true,
-        touchDrag: true,
-        lazyLoad: true,
-		items: 4
-		});
 	
 	
 	$("#home-v1-testimonial-slider").owlCarousel({
@@ -97,12 +87,73 @@ $(document).ready(function(){
         });
 
         $('#home-v1-gallery-slider .owl-next').click(function(){
-            $('#home-v1-gallery-slider .gallery-slider').trigger('owl.next');
+           // $('#home-v1-gallery-slider .gallery-slider').trigger('owl.next');
         })
         $('#home-v1-gallery-slider .owl-prev').click(function(){
-            $('#home-v1-gallery-slider .gallery-slider').trigger('owl.prev');
+            //$('#home-v1-gallery-slider .gallery-slider').trigger('owl.prev');
         });
-    }
+    };
+	
+	if ($('#trainers-gallery-slider').length) {
+        $('#trainers-gallery-slider .trainer-slider').owlCarousel({
+            items: 4,
+            itemsDesktop: [1200, 4],
+            itemsDesktopSmall: [991, 3],
+            itemsTablet: [600, 2],
+            itemsTabletSmall: [560, 1],
+            itemsMobile: false,
+            stopOnHover: true,
+            slideSpeed: 400,
+            pagination: false,
+            mouseDrag: true,
+            touchDrag: true,
+			lazyLoad: true,
+			dots: false,
+			
+			responsiveClass:true,
+				responsive:{
+					0:{
+						items:1,
+						nav : true,
+						
+						navText: ["",""]
+					},
+					560:{
+						items:1,
+						nav : true,
+						
+						navText: ["",""]
+					},
+					600:{
+						items:2,
+						nav : true,
+						
+						navText: ["",""]
+					},
+					991:{
+						items:3,
+						nav : true,
+						
+						navText: ["",""]
+					},
+					1200:{
+						items:4,
+						nav : true,
+						
+						navText: ["",""]
+					},
+				}
+        });
+
+        $('#trainers-gallery-slider .owl-next').click(function(){
+            $('#trainers-gallery-slider .trainer-slider').trigger('owl.next');
+        })
+        $('#trainers-gallery-slider .owl-prev').click(function(){
+            $('#trainers-gallery-slider .trainer-slider').trigger('owl.prev');
+        });
+    };
+	
+	
 });
 
 
